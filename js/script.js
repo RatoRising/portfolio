43f396c77f4c6c3de444fb76ebf90a5a70c882f2
RatoRising <reinaneves@hotmail.com>
2021-08-09 19:07:35 -0300

    revisão de texto e responsividade

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-
 document.querySelector(".menuMobile").addEventListener("click", carregarMenu)
+
 function carregarMenu() {
-        if (document.querySelector(".menu nav ul").style.display == 'flex') {
-            document.querySelector(".menu nav ul").style.display = 'none';
-        } else {
-            document.querySelector(".menu nav ul").style.display = 'flex';
-        }
+    if (document.querySelector(".menu nav ul").style.display == 'flex') {
+        document.querySelector(".menu nav ul").style.display = '';
+    } else {
+        document.querySelector(".menu nav ul").style.display = 'flex';
+    }
 }
 
 document.getElementsByClassName("botaomenu")[0].addEventListener('click', esconderMenu)
@@ -13,6 +13,7 @@ document.getElementsByClassName("botaomenu")[1].addEventListener('click', escond
 document.getElementsByClassName("botaomenu")[2].addEventListener('click', esconderMenu)
 document.getElementsByClassName("botaomenu")[3].addEventListener('click', esconderMenu)
 document.getElementsByClassName("botaomenu")[4].addEventListener('click', esconderMenu)
+
 function esconderMenu() {
-    document.querySelector(".menu nav ul").style.display = 'none'
-}
+        document.querySelector(".menu nav ul").style.display = ''
+    }
