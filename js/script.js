d6ca2259aabdaa24edfc5cef3b167c576dfea81b
RatoRising <reinaneves@hotmail.com>
2022-08-03 12:56:03 -0300

    improves js script

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
-document.querySelector(".menuMobile").addEventListener("click", mostrarMenu)
+const menuNavUl = document.querySelector('ul');
 
-function mostrarMenu() {
-    if (document.querySelector(".menu nav ul").style.display == 'flex') {
-        document.querySelector(".menu nav ul").style.display = '';
-    } else {
-        document.querySelector(".menu nav ul").style.display = 'flex';
-    }
-}
+document.querySelector('.menuMobile').addEventListener('click', () => {
+	menuNavUl.style.display === 'flex' ? (menuNavUl.style.display = '') : (menuNavUl.style.display = 'flex');
+});
 
-document.getElementsByClassName("botaomenu")[0].addEventListener('click', esconderMenu)
-document.getElementsByClassName("botaomenu")[1].addEventListener('click', esconderMenu)
-document.getElementsByClassName("botaomenu")[2].addEventListener('click', esconderMenu)
-document.getElementsByClassName("botaomenu")[3].addEventListener('click', esconderMenu)
-document.getElementsByClassName("botaomenu")[4].addEventListener('click', esconderMenu)
-
-function esconderMenu() {
-        document.querySelector(".menu nav ul").style.display = ''
-    }
+document.querySelectorAll('.menu-items').forEach((btn) => {
+	btn.addEventListener('click', () => (menuNavUl.style.display = ''));
+});
