01cb9020f53aa025901613f9fbc30c5aeabcd032
RatoRising <reinaneves@hotmail.com>
2023-01-26 21:56:46 -0300

    changes menu behavior and images animation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
-const menuNavUl = document.querySelector('ul');
-const menuMobile = document.querySelector('.menu_mobile')
+const menuNavUl = document.querySelector('.menu_navbar__ul');
+
+function transform_menu_icon(x) {
+	x.classList.toggle('transform_menu_icon');
+}
+
+// function addClass(y) {
+// 	y.classList.toggle('add_class');r
+// }
 
 document.querySelector('.menu_mobile').addEventListener('click', () => {
-	menuNavUl.style.display === 'flex' ? (menuNavUl.style.display = '') : (menuNavUl.style.display = 'flex');
+	let menuOpened = menuNavUl.style.left === '0'
+
+	if (!menuOpened) {
+		menuNavUl.style.left = '0';
+	} else {
+		menuNavUl.style.left = '100vw';
+	}
 });
 
 document.querySelectorAll('.menu-items').forEach((btn) => {
-	btn.addEventListener('click', () => (menuNavUl.style.display = ''));
+	btn.addEventListener('click', () => (menuNavUl.style.left = '100vw'));
 });
+
+(function footerDate() {
+	let year = new Date().getFullYear();
+	document.querySelector('#footer_date').innerHTML = year;
+})();
